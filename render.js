505c02007d82d1247c85b0614c88b3e3188da375
NewBobPlus <rschuhler@hotmail.fr>
2020-05-07 15:32:54 +0200

    inspector selection

--- render.js
+++ render.js
@@ -5,31 +5,44 @@ const inspectorContents = categoryNames.map(name =>
   fs.readFileSync(`./menus/${name}.html`, 'utf8')
 );
 
-// Load the category panel
-(() => {
-  let innerHTML = "";
-  categoryNames.forEach((name) => {
-    innerHTML += `<div class="category-name">${name}</div>`;
-  })
-
-  let element = document.getElementsByClassName("categories")[0];
-  element.innerHTML = innerHTML;
-})();
-
 // Load the inspector for each category
 const inspectors = (() => {
-  let inspector = document.getElementsByClassName("inspector")[0];
+  let inspector = document.getElementsByClassName("inspector-panel")[0];
 
-  return inspectorContents.map((content, i) => {
-    let name = categoryNames[i];
+  return inspectorContents.map((content) => {
     let element = document.createElement("div");
     element.innerHTML = content;
     element.classList.add("hidden");
+    element.classList.add("inspector");
     inspector.appendChild(element);
 
-    return {
-      name: name,
-      element: element
-    };
+    return element;
+  });
+})();
+
+// Hide current inspector and show the selected one
+let selectedCategory;
+const showInspector = (i) => {
+  if (selectedCategory != undefined) {
+    inspectors[selectedCategory].classList.add("hidden");
+    categories[selectedCategory].classList.remove("selected");
+  }
+  selectedCategory = i;
+  inspectors[selectedCategory].classList.remove("hidden");
+  categories[selectedCategory].classList.add("selected");
+};
+
+// Load the category panel
+const categories = (() => {
+  let categoriesPanel = document.getElementsByClassName("categories")[0];
+
+  return categoryNames.map((name, i) => {
+    let element = document.createElement("div");
+    element.innerHTML = name;
+    element.classList.add("category-name");
+    element.onclick = () => showInspector(i);
+    categoriesPanel.appendChild(element);
+
+    return element;
   });
 })();
